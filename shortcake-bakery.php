8e552aaf0b0abd702518f0a6a94ae8f938b75f0b
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-21 10:31:16 -0700

    Initial commit of scaffolding code

---
+++ shortcake-bakery.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Shortcake Bakery
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: shortcake-bakery
+Domain Path: /languages
+*/
+
