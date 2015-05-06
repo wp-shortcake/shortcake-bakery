8194bec858ae4388af2b025a42170572bf6107ca
Daniel Bachhuber <d@danielbachhuber.com>
2015-05-06 07:16:42 -0700

    Fix path for autoloader

--- inc/class-shortcake-bakery.php
+++ inc/class-shortcake-bakery.php
@@ -36,7 +36,7 @@ class Shortcake_Bakery {
 		$last = array_pop( $parts ); // File should be 'class-[...].php'
 		$last = 'class-' . $last . '.php';
 		$parts[] = $last;
-		$file = dirname( __FILE__ ) . '/inc/' . str_replace( '_', '-', strtolower( implode( $parts, '/' ) ) );
+		$file = dirname( __FILE__ ) . '/shortcodes/' . str_replace( '_', '-', strtolower( implode( $parts, '/' ) ) );
 		if ( file_exists( $file ) ) {
 			require $file;
 		}
