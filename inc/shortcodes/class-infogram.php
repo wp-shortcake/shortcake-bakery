ecab6d3ec6cc75a26619f5b9f70c16fbd3f913b7
davisshaver <davisshaver@users.noreply.github.com>
2015-06-16 14:54:11 -0400

    First pass at Infogram shortcode

---
+++ inc/shortcodes/class-infogram.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Shortcake_Bakery\Shortcodes;
+
+class Facebook extends Shortcode {
+
+	public static function get_shortcode_ui_args() {
+		return array(
+			'label'          => esc_html__( 'Infogram', 'shortcake-bakery' ),
+			'listItemImage'  => 'TK',
+			'attrs'          => array(
+				array(
+					'label'        => esc_html__( 'URL', 'shortcake-bakery' ),
+					'attr'         => 'url',
+					'type'         => 'text',
+					'description'  => esc_html__( 'URL to the Infogram', 'shortcake-bakery' ),
+				),
+			),
+		);
+	}
+
+	public static function setup_actions() {
+		add_action( 'init', 'Shortcake_Bakery\Shortcodes\Infogram::action_init_register_scripts' );
+	}
+
+	public static function action_init_register_scripts() {
+		wp_register_script( 'infogram-api', '//e.infogr.am/js/embed.js' );
+	}
+
+	public static function reversal( $content ) {
+		if ( preg_match_all( '#<script id="[^<]+" src="//e\.infogr\.am/js/embed\.js\?[^>]+" type="text/javascript"></script>?#', $content, $matches ) ) {
+			$replacements = array();
+			$shortcode_tag = self::get_shortcode_tag();
+			foreach ( $matches[0] as $key => $value ) {
+				$replacements[ $value ] = '[' . $shortcode_tag . ' url="' . esc_url( $matches[1][ $key ] ) . '"]';
+			}
+			$content = str_replace( array_keys( $replacements ), array_values( $replacements ), $content );
+		}
+		return $content;
+	}
+
+	public static function callback( $attrs, $content = '' ) {
+
+		if ( empty( $attrs['url'] ) ) {
+			return '';
+		}
+
+		$out = '<div id="infogram_0_washington_marijuana_sales"></div>';
+		wp_enqueue_script( 'infogram-api' );
+		return $out;
+	}
+
+}
---
+++ tests/test-infogram-shortcode.php
@@ -0,0 +1,28 @@
+<?php
+
+class Test_Infogram_Shortcode extends WP_UnitTestCase {
+
+	public function test_post_display() {
+		$post_id = $this->factory->post->create( array( 'post_content' => '[infogram url="http://infogr.am/washington_marijuana_sales"]' ) );
+		$post = get_post( $post_id );
+		$this->assertContains( '<div id="infogram_0_washington_marijuana_sales"></div><script src="//e.infogr.am/js/embed.js" type="text/javascript"></script>', apply_filters( 'the_content', $post->post_content ) );
+	}
+
+	public function test_embed_reversal() {
+		$old_content = <<<EOT
+
+		apples before
+
+		<script id="infogram_0_washington_marijuana_sales" src="//e.infogr.am/js/embed.js?hbf" type="text/javascript"></script>
+
+		bananas after
+EOT;
+		$transformed_content = wp_filter_post_kses( $old_content );
+		$transformed_content = str_replace( '\"', '"', $transformed_content ); // Kses slashes the data
+		$this->assertContains( '[infogram url="http://infogr.am/washington_marijuana_sales"]', $transformed_content );
+		$this->assertContains( 'apples before', $transformed_content );
+		$this->assertContains( 'bananas after', $transformed_content );
+
+	}
+
+}
