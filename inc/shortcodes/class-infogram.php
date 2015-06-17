fb0fe2edff7bc49fc4c9f36a891964a6e705f2b8
davisshaver <davisshaver@users.noreply.github.com>
2015-06-16 20:57:01 -0400

    Get shortcode reversal working properly

--- inc/shortcodes/class-infogram.php
+++ inc/shortcodes/class-infogram.php
@@ -24,15 +24,18 @@ class Infogram extends Shortcode {
 	}
 
 	public static function action_init_register_scripts() {
-		wp_register_script( 'infogram-api', 'http://e.infogr.am/js/embed.js' );
+		wp_register_script( 'infogram-api', '//e.infogr.am/js/embed.js' );
 	}
 
 	public static function reversal( $content ) {
 		if ( preg_match_all( '#<script id="[^<]+" src="//e\.infogr\.am/js/embed\.js\?[^>]+" type="text/javascript"></script>?#', $content, $matches ) ) {
 			$replacements = array();
 			$shortcode_tag = self::get_shortcode_tag();
-			foreach ( $matches[0] as $key => $value ) {
-				$replacements[ $value ] = '[' . $shortcode_tag . ' url="https://infogram.com/' . esc_url( $matches[1][ $key ] ) . '"]';
+			foreach ( $matches[0] as $key=>$value) {
+				$parts = explode('"', $value);
+				$id = $parts[1];
+				$url_string = str_replace( 'infogram_0_' , '', $id );
+				$replacements[ $value ] = '[' . $shortcode_tag . ' url="http://infogr.am/' . $url_string . '"]';
 			}
 			$content = str_replace( array_keys( $replacements ), array_values( $replacements ), $content );
 		}
