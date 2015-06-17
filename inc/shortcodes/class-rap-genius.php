5b6b5c5e173458c7db3ef4da9b3d2fff4f420d00
davisshaver <davisshaver@users.noreply.github.com>
2015-06-17 07:31:04 -0400

    Moves Rap Genius to Shortcake Bakery

---
+++ inc/shortcodes/class-rap-genius.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Shortcake_Bakery\Shortcodes;
+
+class Rap_Genius extends Shortcode {
+
+	public static function get_shortcode_ui_args() {
+		return array(
+			'label'          => esc_html__( 'Rap Genius', 'shortcake-bakery' ),
+			'listItemImage'  => 'RAP',
+		);
+	}
+
+	/**
+	 * Render the shortcode on-demand
+	 *
+	 * @param array $attrs
+	 * @param string $content
+	 */
+	public static function callback( $attrs, $content = '' ) {
+		return '<script async src="https://genius.codes"></script>';
+	}
+
+	public static function reversal( $content ) {
+		if ( preg_match_all( '#<script async src="https://genius.codes"></script>#', $content, $matches ) ) {
+			$replacements = array();
+			$shortcode_tag = self::get_shortcode_tag();
+			foreach ( $matches[0] as $key => $value ) {
+				$replacements[ $value ] = '[' . $shortcode_tag . ']';
+			}
+			$content = str_replace( array_keys( $replacements ), array_values( $replacements ), $content );
+		}
+		return $content;
+	}
+
+}
