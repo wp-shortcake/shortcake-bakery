e87a075742d649bcbd47dc4916a5b6473311d647
davisshaver <davis@daviss-MacBook.local>
2015-07-08 09:31:19 -0400

    Migrate script shortcake and add whitelist filter

---
+++ inc/shortcodes/class-script.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Shortcake_Bakery\Shortcodes;
+
+class Script extends Shortcode {
+
+	private static $whitelisted_script_domains = array();
+
+	/**
+	 * Get the whitelisted script domains for the plugin
+	 */
+	public static function get_whitelisted_script_domains() {
+		return apply_filters( 'shortcake_bakery_whitelisted_script_domains', self::$whitelisted_script_domains );
+	}
+
+	public static function reversal( $content ) {
+
+		$whitelisted_script_domains = self::get_whitelisted_script_domains();
+		$content = preg_replace_callback( '!\<script\s[^>]*?src=[\"\']([^\"\']+)[\"\'][^>]*?\>\s{0,}\</script\>!i', function( $match ) use ( $whitelisted_script_domains ) {
+
+				$url = ( 0 === strpos( $match[1], '//' ) ) ? 'http:' . $match[1] : $match[1];
+				$host = parse_url( $url, PHP_URL_HOST );
+				if ( ! in_array( $host, $whitelisted_script_domains ) ) {
+					return $match[0];
+				}
+				$shortcode_tag = self::get_shortcode_tag();
+				$replacement = '[' . $shortcode_tag . ' src="' . esc_url( $match[1] ) . '"][/' . $shortcode_tag . ']';
+				return $replacement;
+		}, $content );
+
+		return $content;
+	}
+
+	public static function callback( $attrs, $content = '' ) {
+
+		if ( empty( $attrs['src'] ) ) {
+			return '';
+		}
+
+		$host = parse_url( $attrs['src'], PHP_URL_HOST );
+		if ( ! in_array( $host, self::get_whitelisted_script_domains() ) ) {
+			return '';
+		}
+
+		return '<script src="' . esc_url( $attrs['src'] ) . '"></script>';
+	}
+
+}
---
+++ tests/test-script-shortcode.php
@@ -0,0 +1,43 @@
+<?php
+
+class Test_Script_Shortcode extends WP_UnitTestCase {
+
+	public function setUp() {
+		parent::setUp();
+		add_filter( 'shortcake_bakery_whitelisted_script_domains', function(){
+		return array(
+				'3vot.com',
+			);
+		});
+
+	}
+
+	public function test_post_display() {
+		$post_id = $this->factory->post->create( array( 'post_content' => '[script src="//3vot.com/fusion/waittimes/3vot.js"][/script]' ) );
+		$post = get_post( $post_id );
+		$this->assertContains( '<script src="//3vot.com/fusion/waittimes/3vot.js"></script>', apply_filters( 'the_content', $post->post_content ) );
+	}
+
+	public function test_display_invalid_domain() {
+		$post_id = $this->factory->post->create( array( 'post_content' => '[script src="//baddomain.com/malicious.js"][/script]' ) );
+		$post = get_post( $post_id );
+		$this->assertEmpty( trim(  apply_filters( 'the_content', $post->post_content ) ) );
+	}
+	public function test_embed_reversal() {
+		$old_content = <<<EOT
+
+		apples before
+
+		<script src="//3vot.com/fusion/waittimes/3vot.js"></script>
+
+		bananas after
+EOT;
+		$transformed_content = wp_filter_post_kses( $old_content );
+		$transformed_content = str_replace( '\"', '"', $transformed_content ); // Kses slashes the data
+		$this->assertContains( '[script src="//3vot.com/fusion/waittimes/3vot.js"][/script]', $transformed_content );
+		$this->assertContains( 'apples before', $transformed_content );
+		$this->assertContains( 'bananas after', $transformed_content );
+
+	}
+
+}
