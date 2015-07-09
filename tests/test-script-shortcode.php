63bf5be6529ca82c67700e929ce51e8ab72d4fb2
davisshaver <davis@daviss-MacBook.local>
2015-07-09 09:12:20 -0400

    Add test for double embed reversal

--- tests/test-script-shortcode.php
+++ tests/test-script-shortcode.php
@@ -7,6 +7,7 @@ class Test_Script_Shortcode extends WP_UnitTestCase {
 		add_filter( 'shortcake_bakery_whitelisted_script_domains', function(){
 			return array(
 				'3vot.com',
+				'ajax.googleapis.com'
 			);
 		});
 	}
@@ -39,6 +40,25 @@ EOT;
 
 	}
 
+	public function test_embed_double_reversal() {
+		$old_content = <<<EOT
+
+		apples before
+
+		<script src="//3vot.com/fusion/waittimes/3vot.js"></script>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+		bananas after
+EOT;
+		$transformed_content = wp_filter_post_kses( $old_content );
+		$transformed_content = str_replace( '\"', '"', $transformed_content ); // Kses slashes the data
+		$this->assertContains( '[script src="http://3vot.com/fusion/waittimes/3vot.js"][/script]', $transformed_content );
+		$this->assertContains( '[script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"][/script]', $transformed_content );
+		$this->assertContains( 'apples before', $transformed_content );
+		$this->assertContains( 'bananas after', $transformed_content );
+
+	}
 	public function test_embed_non_reversal() {
 		$old_content = '<script src="//baddomain.com/malicious.js"></script>';
 		$transformed_content = wp_filter_post_kses( $old_content );
