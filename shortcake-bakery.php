36fa1e914357f105726a8d8f973485d61bf6e248
Daniel Bachhuber <d@danielbachhuber.com>
2015-07-17 07:06:38 -0700

    Version bump

--- shortcake-bakery.php
+++ shortcake-bakery.php
@@ -12,7 +12,7 @@ Domain Path: /languages
 
 require_once dirname( __FILE__ ) . '/inc/class-shortcake-bakery.php';
 
-define( 'SHORTCAKE_BAKERY_VERSION', '0.1-alpha' );
+define( 'SHORTCAKE_BAKERY_VERSION', '0.1.0' );
 define( 'SHORTCAKE_BAKERY_URL_ROOT', plugin_dir_url( __FILE__ ) );
 /**
  * Load the Shortcake Bakery
