54d50621b653a6cc47ca6702329d62c89101259b
Daniel Bachhuber <d@danielbachhuber.com>
2015-07-22 06:05:06 -0700

    Version bump

--- shortcake-bakery.php
+++ shortcake-bakery.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Shortcake Bakery
-Version: 0.1.0
+Version: 0.2.0-alpha
 Description: A fine selection of Shortcake-powered shortcodes.
 Author: fusionengineering, davisshaver, danielbachhuber
 Author URI: http://www.fusion.net/section/tech-product/
@@ -12,7 +12,7 @@ Domain Path: /languages
 
 require_once dirname( __FILE__ ) . '/inc/class-shortcake-bakery.php';
 
-define( 'SHORTCAKE_BAKERY_VERSION', '0.1.0' );
+define( 'SHORTCAKE_BAKERY_VERSION', '0.2.0-alpha' );
 define( 'SHORTCAKE_BAKERY_URL_ROOT', plugin_dir_url( __FILE__ ) );
 /**
  * Load the Shortcake Bakery
