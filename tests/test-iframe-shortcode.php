b503bcc265ada5984f34507b16bb16fa0ea03875
davisshaver <davis@daviss-MacBook.local>
2015-07-22 14:46:50 -0500

    Test iframe shortcake for height param passage

--- tests/test-iframe-shortcode.php
+++ tests/test-iframe-shortcode.php
@@ -19,6 +19,12 @@ class Test_Iframe_Shortcode extends WP_UnitTestCase {
 		$this->assertContains( '<iframe src="//static.fusion.net/the-ultimate-choice/" width="100%" height="600" frameborder="0" scrolling="no" class="shortcake-bakery-responsive"></iframe>', apply_filters( 'the_content', $post->post_content ) );
 	}
 
+	public function test_iframe_height_parameter() {
+		$post_id = $this->factory->post->create( array( 'post_content' => '[iframe height="900" src="//static.fusion.net/the-ultimate-choice/"]' ) );
+		$post = get_post( $post_id );
+		$this->assertContains( '<iframe src="//static.fusion.net/the-ultimate-choice/" width="100%" height="900" frameborder="0" scrolling="no" class="shortcake-bakery-responsive"></iframe>', apply_filters( 'the_content', $post->post_content ) );
+	}
+
 	public function test_display_invalid_domain() {
 		$post_id = $this->factory->post->create( array( 'post_content' => '[iframe src="http://notvalid.fusion.net/leonardo-dicaprio/"]' ) );
 		$post = get_post( $post_id );
