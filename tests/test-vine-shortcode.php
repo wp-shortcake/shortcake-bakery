2214a9e0e808361429a9b54dad5a91b45f389546
Daniel Bachhuber <d@danielbachhuber.com>
2015-09-25 08:21:18 -0700

    Failing test case for Vine embed with trailing `&nbsp;`

--- tests/test-vine-shortcode.php
+++ tests/test-vine-shortcode.php
@@ -36,4 +36,12 @@ EOT;
 		$this->assertEquals( $expected_content, $transformed_content );
 	}
 
+	public function test_embed_reversal_with_trailing_nbsp() {
+		$old_content = '<iframe src="https://vine.co/v/eMnmixYtwr1/embed/simple" width="300" height="300" frameborder="0">&nbsp;';
+		$expected_content = '[vine url="https://vine.co/v/eMnmixYtwr1"]';
+		$transformed_content = wp_filter_post_kses( $old_content );
+		$transformed_content = str_replace( '\"', '"', $transformed_content ); // Kses slashes the data
+		$this->assertEquals( $expected_content, $transformed_content );
+	}
+
 }
