63c215a31d4e6bdce118e9d518aa577c58fc0c93
Daniel Bachhuber <d@danielbachhuber.com>
2015-09-25 08:27:01 -0700

    Update Vine reversal to handle missing closing `</iframe>` tag

--- tests/test-vine-shortcode.php
+++ tests/test-vine-shortcode.php
@@ -36,9 +36,9 @@ EOT;
 		$this->assertEquals( $expected_content, $transformed_content );
 	}
 
-	public function test_embed_reversal_with_trailing_nbsp() {
+	public function test_embed_reversal_with_no_closing_tag_and_trailing_nbsp() {
 		$old_content = '<iframe src="https://vine.co/v/eMnmixYtwr1/embed/simple" width="300" height="300" frameborder="0">&nbsp;';
-		$expected_content = '[vine url="https://vine.co/v/eMnmixYtwr1"]';
+		$expected_content = '[vine url="https://vine.co/v/eMnmixYtwr1"]&nbsp;';
 		$transformed_content = wp_filter_post_kses( $old_content );
 		$transformed_content = str_replace( '\"', '"', $transformed_content ); // Kses slashes the data
 		$this->assertEquals( $expected_content, $transformed_content );
