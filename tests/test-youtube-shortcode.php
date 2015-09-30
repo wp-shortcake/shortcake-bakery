9f216c9f102d24bef5be56b6af51877a08cd6b8c
Daniel Bachhuber <d@danielbachhuber.com>
2015-09-30 06:22:20 -0700

    Support filtering YouTube embed URL
    This makes it easier for themes and plugins to modify the embed

--- tests/test-youtube-shortcode.php
+++ tests/test-youtube-shortcode.php
@@ -14,6 +14,20 @@ class Test_YouTube_Shortcode extends WP_UnitTestCase {
 		$this->assertContains( '<iframe class="shortcake-bakery-responsive" width="640" height="360" src="https://youtube.com/embed/r34ust62leA?list=PLxd0bZ1RXEzvZnP-sC7Byj_a1dFGHF3xt" frameborder="0"></iframe>', apply_filters( 'the_content', $post->post_content ) );
 	}
 
+	public function test_youtube_embed_url_filter() {
+		$post_id = $this->factory->post->create( array( 'post_content' => '[youtube url="https://www.youtube.com/watch?v=hDlpVFDmXrc"]' ) );
+		$post = get_post( $post_id );
+		$filter = function( $embed_url ) {
+			return add_query_arg( array(
+				'rel'       => 0,
+				'showinfo'  => 0
+				), $embed_url );
+		};
+		add_filter( 'shortcake_bakery_youtube_embed_url', $filter );
+		$this->assertContains( '<iframe class="shortcake-bakery-responsive" width="640" height="360" src="https://youtube.com/embed/hDlpVFDmXrc?rel=0&#038;showinfo=0" frameborder="0"></iframe>', apply_filters( 'the_content', $post->post_content ) );
+		remove_filter( 'shortcake_bakery_youtube_embed_url', $filter );
+	}
+
 	public function test_embed_reversal() {
 		$old_content = <<<EOT
 		apples before
