5b73d86e73130c72f5315474a5b8170c2e74a0c1
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-12-18 16:31:54 -0500

    Add "script" type for ABC News shortcode
    Adds a "type" parameter to ABC News shortcode. If not set, defaults to
    "iframe", which was the previous representation.

--- inc/shortcodes/class-abc-news.php
+++ inc/shortcodes/class-abc-news.php
@@ -12,6 +12,16 @@ class ABC_News extends Shortcode {
 			'listItemImage'  => '<img src="' . esc_url( SHORTCAKE_BAKERY_URL_ROOT . 'assets/images/svg/icon-abc-news.svg' ) . '" />',
 			'attrs'          => array(
 				array(
+					'label'        => esc_html__( 'Type', 'shortcake-bakery' ),
+					'attr'         => 'type',
+					'type'         => 'select',
+					'options'      => array(
+						'iframe' => esc_html__( 'Iframe', 'shortcake-bakery' ),
+						'script' => esc_html__( 'Script tag', 'shortcake-bakery' ),
+					),
+					'description'  => esc_html__( 'URL to a ABC News video', 'shortcake-bakery' ),
+				),
+				array(
 					'label'        => esc_html__( 'URL', 'shortcake-bakery' ),
 					'attr'         => 'url',
 					'type'         => 'text',
@@ -29,7 +39,18 @@ class ABC_News extends Shortcode {
 				if ( ! in_array( self::parse_url( $iframe->attrs['src'], PHP_URL_HOST ), self::$valid_hosts ) ) {
 					continue;
 				}
-				$replacements[ $iframe->original ] = '[' . self::get_shortcode_tag() . ' url="' . esc_url_raw( $iframe->attrs['src'] ) . '"]';
+				$replacements[ $iframe->original ] = '[' . self::get_shortcode_tag() . ' type="iframe" url="' . esc_url_raw( $iframe->attrs['src'] ) . '"]';
+			}
+			$content = self::make_replacements_to_content( $content, $replacements );
+		}
+
+		if ( $scripts = self::parse_scripts( $content ) ) {
+			$replacements = array();
+			foreach ( $scripts as $script ) {
+				if ( ! in_array( self::parse_url( $script->attrs['src'], PHP_URL_HOST ), self::$valid_hosts ) ) {
+					continue;
+				}
+				$replacements[ $script->original ] = '[' . self::get_shortcode_tag() . ' type="script" url="' . esc_url_raw( $script->attrs['src'] ) . '"]';
 			}
 			$content = self::make_replacements_to_content( $content, $replacements );
 		}
@@ -38,12 +59,18 @@ class ABC_News extends Shortcode {
 	}
 
 	public static function callback( $attrs, $content = '' ) {
+		$attrs = wp_parse_args( $attrs, array( 'type' => 'iframe' ) );
 
 		if ( empty( $attrs['url'] ) || ! in_array( self::parse_url( $attrs['url'], PHP_URL_HOST ), self::$valid_hosts ) ) {
 			return '';
 		}
 
-		return sprintf( '<iframe class="shortcake-bakery-responsive" width="640" height="480" src="%s" frameborder="0"></iframe>', esc_url( $attrs['url'] ) );
+		switch ( $attrs['type'] ) {
+			case 'iframe':
+				return sprintf( '<iframe class="shortcake-bakery-responsive" width="640" height="480" src="%s" frameborder="0"></iframe>', esc_url( $attrs['url'] ) );
+			case 'script':
+				return sprintf( '<script src="%s"></script>', esc_url( $attrs['url'] ) );
+		}
 	}
 
 }
