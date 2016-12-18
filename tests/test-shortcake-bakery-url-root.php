5298ccde957ff791cb74706bd3c211515c255179
Davis Shaver <davis@alleyinteractive.com>
2016-12-18 14:54:24 -0500

    Update test

--- tests/test-shortcake-bakery-url-root.php
+++ tests/test-shortcake-bakery-url-root.php
@@ -8,7 +8,7 @@ class Test_Shortcake_Bakery_URL_Root extends WP_UnitTestCase {
 
 
 	public function test_url_root() {
-		$this->assertContains( SHORTCAKE_BAKERY_URL_ROOT, 'http://www.example.org/wp-content/plugins/shortcake-bakery/' );
+		$this->assertContains( SHORTCAKE_BAKERY_URL_ROOT, 'http://example.org/wp-content/plugins/shortcake-bakery/' );
 	}
 
 }
