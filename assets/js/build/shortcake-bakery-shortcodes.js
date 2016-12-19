fe4368e31afc48cddfc74a1a8975253998627741
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2016-12-19 11:37:58 -0800

    Re-run grunt after updating browserify-shim

--- assets/js/build/shortcake-bakery-shortcodes.js
+++ assets/js/build/shortcake-bakery-shortcodes.js
@@ -1,10 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
-var _                  = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null);
-var $                  = (typeof window !== "undefined" ? window.jQuery : typeof global !== "undefined" ? global.jQuery : null);
-var wp                 = (typeof window !== "undefined" ? window.wp : typeof global !== "undefined" ? global.wp : null);
-var Backbone           = (typeof window !== "undefined" ? window.Backbone : typeof global !== "undefined" ? global.Backbone : null);
-var ShortcakeBakery    = (typeof window !== "undefined" ? window.ShortcakeBakery : typeof global !== "undefined" ? global.ShortcakeBakery : null);
+var _                  = (typeof window !== "undefined" ? window['_'] : typeof global !== "undefined" ? global['_'] : null);
+var $                  = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
+var wp                 = (typeof window !== "undefined" ? window['wp'] : typeof global !== "undefined" ? global['wp'] : null);
+var Backbone           = (typeof window !== "undefined" ? window['Backbone'] : typeof global !== "undefined" ? global['Backbone'] : null);
+var ShortcakeBakery    = (typeof window !== "undefined" ? window['ShortcakeBakery'] : typeof global !== "undefined" ? global['ShortcakeBakery'] : null);
 
 /**
  * Support JS for Shortcake Bakery shortcodes.
