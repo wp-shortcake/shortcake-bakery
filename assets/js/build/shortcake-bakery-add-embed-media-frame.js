521b443a62f1ba4aff36a0610247c5a6f1777598
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2016-12-19 11:04:20 -0800

    Run build with new NPM packages
    Updates the the stylesheet sourcemaps, and reverts the change to global
    imports that happened in #196.

--- assets/js/build/shortcake-bakery-add-embed-media-frame.js
+++ assets/js/build/shortcake-bakery-add-embed-media-frame.js
@@ -1,8 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
-var wp = (typeof window !== "undefined" ? window['wp'] : typeof global !== "undefined" ? global['wp'] : null);
-var Backbone = (typeof window !== "undefined" ? window['Backbone'] : typeof global !== "undefined" ? global['Backbone'] : null);
-var $ = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
+var wp = (typeof window !== "undefined" ? window.wp : typeof global !== "undefined" ? global.wp : null);
+var Backbone = (typeof window !== "undefined" ? window.Backbone : typeof global !== "undefined" ? global.Backbone : null);
+var $ = (typeof window !== "undefined" ? window.jQuery : typeof global !== "undefined" ? global.jQuery : null);
 
 var addEmbedController = wp.media.controller.State.extend({
 
@@ -56,9 +56,9 @@ module.exports = addEmbedController;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
 (function (global){
-var _               = (typeof window !== "undefined" ? window['_'] : typeof global !== "undefined" ? global['_'] : null);
-var wp              = (typeof window !== "undefined" ? window['wp'] : typeof global !== "undefined" ? global['wp'] : null);
-var ShortcakeBakery = (typeof window !== "undefined" ? window['ShortcakeBakery'] : typeof global !== "undefined" ? global['ShortcakeBakery'] : null);
+var _               = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null);
+var wp              = (typeof window !== "undefined" ? window.wp : typeof global !== "undefined" ? global.wp : null);
+var ShortcakeBakery = (typeof window !== "undefined" ? window.ShortcakeBakery : typeof global !== "undefined" ? global.ShortcakeBakery : null);
 
 var addEmbedToolbar = wp.media.view.Toolbar.extend({
 	initialize : function() {
@@ -100,7 +100,7 @@ module.exports = addEmbedToolbar;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],3:[function(require,module,exports){
 (function (global){
-var wp = (typeof window !== "undefined" ? window['wp'] : typeof global !== "undefined" ? global['wp'] : null);
+var wp = (typeof window !== "undefined" ? window.wp : typeof global !== "undefined" ? global.wp : null);
 
 var addEmbedView = wp.media.View.extend({
 	className: 'media-add-embed',
@@ -167,11 +167,11 @@ module.exports = addEmbedView;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],4:[function(require,module,exports){
 (function (global){
-var _                  = (typeof window !== "undefined" ? window['_'] : typeof global !== "undefined" ? global['_'] : null);
-var $                  = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
-var wp                 = (typeof window !== "undefined" ? window['wp'] : typeof global !== "undefined" ? global['wp'] : null);
-var Backbone           = (typeof window !== "undefined" ? window['Backbone'] : typeof global !== "undefined" ? global['Backbone'] : null);
-var ShortcakeBakery    = (typeof window !== "undefined" ? window['ShortcakeBakery'] : typeof global !== "undefined" ? global['ShortcakeBakery'] : null);
+var _                  = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null);
+var $                  = (typeof window !== "undefined" ? window.jQuery : typeof global !== "undefined" ? global.jQuery : null);
+var wp                 = (typeof window !== "undefined" ? window.wp : typeof global !== "undefined" ? global.wp : null);
+var Backbone           = (typeof window !== "undefined" ? window.Backbone : typeof global !== "undefined" ? global.Backbone : null);
+var ShortcakeBakery    = (typeof window !== "undefined" ? window.ShortcakeBakery : typeof global !== "undefined" ? global.ShortcakeBakery : null);
 var addEmbedView       = require('./addEmbed/view');
 var addEmbedToolbar    = require('./addEmbed/toolbar');
 var addEmbedController = require('./addEmbed/controller');
