938fd9996cf05bd89a38ac368f67b11e11350569
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2017-04-28 10:07:49 -0700

    Ignore code sniffs for filesystem operations
    There's no way that I'm aware of accomplishing this through the
    WP_Filesystem class, since WP_Filesystem doesn't allow passing in a
    stream context. So I'm just going to ignore the code sniffs for this
    block. (Since it's disabled by default, I think this should be fine.)

--- inc/class-asset-proxy.php
+++ inc/class-asset-proxy.php
@@ -50,6 +50,11 @@ class Asset_Proxy {
 		);
 		$stream_context = stream_context_create( $stream_options );
 
+		// @codingStandardsIgnoreStart
+		// (Note: the fopen here happen in a strem context, so that we can't use the WP_Filesystem class.
+		// Because we're opening the remote file in a stream, it also isn't
+		// required that the allow_url_fopen directive be enabled in PHP for
+		// this to work.)
 		$input_handle = fopen( esc_url_raw( $asset_url ), 'r', null, $stream_context );
 		$output_handle = fopen( 'php://output', 'w+', null, $stream_context );
 
@@ -60,6 +65,8 @@ class Asset_Proxy {
 		}
 
 		stream_copy_to_stream( $input_handle, $output_handle );
+		// @codingStandardsIgnoreEnd
+
 		exit;
 	}
 }
